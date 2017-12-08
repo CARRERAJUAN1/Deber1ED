0ca9a0d2fc314cd8806c2a318850b6ccabd2a564
juanCarrera <https://github.com/dashboard?fork=0>
2017-12-07 21:44:04 -0500

    programa en java deber #1

---
+++ src/ec/edu/ister/modelo/ProgramaSaludo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ister.modelo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author PC
+ */
+public class ProgramaSaludo {
+    public  void HolaMundo() {
+        JOptionPane.showInputDialog("Hola Mundo");
+        
+    }
+    
+}
